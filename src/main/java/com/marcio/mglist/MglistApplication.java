6bb2b672a1e3f9b14fbd9dbbaf52f1f3903f2572
Marcio Gabriel <gabriel09msn@gmail.com>
2023-05-26 17:47:23 -0300

    Project created

---
+++ src/main/java/com/marcio/mglist/MglistApplication.java
@@ -0,0 +1,13 @@
+package com.marcio.mglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MglistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MglistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcio/mglist/MglistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcio.mglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MglistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
